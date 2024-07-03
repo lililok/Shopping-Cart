aeb1dcd2f7c78071ffeb8b76ec6dc63792f02e82
Lililok <eshanu.sofia@yandex.ru>
2024-07-03 21:20:44 +0300

    image fix second try

--- src/parts/Banner.jsx
+++ src/parts/Banner.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import storeData from "../fetch.jsx";
 import Card from "./Card.jsx";
+import firstBanner from '../assets/firstBanner.jpg';
+import secondBanner from '../assets/secondBanner.jpg';
 
 function Banner() {
     const { top2Data } = storeData();
@@ -10,7 +12,7 @@ function Banner() {
         <>
         <div className="flex flex-col items-center p-6">
             <div className='flex items-center bg-gray-800 p-6'>
-                <img src="/src/assets/firstBanner.jpg" alt="banner" className='object-contain max-w-screen-sm'/>
+                <img src={firstBanner} alt="banner" className='object-contain max-w-screen-sm'/>
                 <div className="flex flex-col items-center m-6">
                     <h3 className='text-white text-xl mb-4'>Top Offer</h3>
                     <div className="flex space-x-4">
@@ -19,7 +21,7 @@ function Banner() {
                     ))}
                     </div>
                 </div>
-                <img src="/src/assets/secondBanner.jpg" alt="banner" className='object-contain max-w-screen-sm'/>
+                <img src={secondBanner} alt="banner" className='object-contain max-w-screen-sm'/>
             </div>
             <button className="px-8 py-4 text-lg bg-yellow-400 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full mt-10"><Link to="shop">See all offers</Link></button>
         </div>
